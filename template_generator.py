c79c04fd9cffd585903ee6e6acbed1d486c4916f
Abhinav <abhinavkumarfzd@gmail.com>
2019-03-03 16:17:54 -0500

    added more files in template generator

--- template_generator.py
+++ template_generator.py
@@ -5,4 +5,6 @@ if __name__ == '__main__':
     dirname = sys.argv[1]
     os.mkdir(dirname)
     open('{0}/output'.format(dirname), 'w').close()
+    open('{0}/input'.format(dirname), 'w').close()
     open('{0}/solution'.format(dirname), 'w').close()
+    open('{0}/{1}.py'.format(dirname, dirname.replace('-', '_')), 'w').close()
